0276031df87f34f1ff24bfbe414dc4bafe678193
constanza30101 <comartinez@alumnos.exa.unicen.edu.ar>
2021-10-13 17:07:21 -0300

    arreglo de errores

--- Controller/MarksController.php
+++ Controller/MarksController.php
@@ -4,147 +4,81 @@
     require_once "./View/MarksView.php";
     require_once "./Model/ProductsModel.php";
     require_once "./Model/MarksModel.php";
+    require_once "./View/LoginView.php";
+    require_once "./Controller/LoginController.php";
 
     class MarksController{
 
-        private $Productsview;
-        private $MarksView;
+        private $productsView;
+        private $marksView;
         private $productosModel;
         private $marksModel;
+        private $loginView;
+        private $loginControl;
 
         function __construct(){
-            $this->Productsview = new ProductsView();
-            $this->MarksView = new MarksView();
-            $thi<?php
-
-            require_once "./View/ProductsView.php";
-            require_once "./View/MarksView.php";
-            require_once "./Model/ProductsModel.php";
-            require_once "./Model/MarksModel.php";
-            require_once "./View/LoginView.php";
-            require_once "./Controller/LoginController.php";
-        
-            class MarksController{
-        
-                private $productsView;
-                private $marksView;
-                private $productosModel;
-                private $marksModel;
-                private $loginView;
-                private $loginControl;
-        
-                function __construct(){
-                    $this->productsView = new ProductsView();
-                    $this->marksView = new MarksView();
-                    $this->productosModel = new ProductsModel();
-                    $this->marksModel = new MarksModel();
-                    $this->loginView = new LoginView();
-                    $this->loginControl = new LoginController();
-                }
-                //LLAMA AL HOME DE MARCAS
-                function HomeMarks(){
-                    $marks = $this->marksModel->GetMarks();
-                    $this->marksView->ShowMarks($marks);
-                }
-                //INSERTA UNA NUEVA MARCA
-                function InsertMark(){
-                    $logeado = $this->loginControl->checkLoggedIn();
-                    if($logeado){
-                        if (isset($_POST['input_mark']) && isset($_POST['input_category'])) {
-                            $mark = $_POST['input_mark'];
-                            $category = $_POST['input_category'];
-                            $this->marksModel->InsertMark($mark, $category);
-                        }
-                        $this->productsView->ShowLocation('admin');
-                    }else{
-                        $this->loginView->ShowLogin();
-                    }
-                }
-                //ELIMINA UNA MARCA POR ID
-                function DeleteMark($params = null){
-                    $logeado = $this->loginControl->checkLoggedIn();
-                    if($logeado){
-                        $mark_id = $params[':ID'];
-                        $this->marksModel->DeleteMark($mark_id);
-                        $this->productsView->ShowLocation('admin');
-                    }else{
-                        $this->loginView->ShowLogin();
-                    }
-                }
-                //LLAMA LA VISTA PARA EDITAR UNA MARCA POR ID
-                function EditMark($params = null){
-                    $logeado = $this->loginControl->checkLoggedIn();
-                    if($logeado){
-                        $mark_id = $params[':ID'];
-                        $mark = $this->marksModel->GetMarkById($mark_id);
-                        $this->marksView->ShowEditMark($mark);
-                    }else{
-                        $this->loginView->ShowLogin();
-                    }
-                }
-                //LLAMA A ACTUALIZAR UNA MARCA
-                function UpdateMark($params = null){
-                    $logeado = $this->loginControl->checkLoggedIn();
-                    if($logeado){
-                        $mark_id = $params[':ID'];
-                        if (isset($_POST['edit_mark']) && isset($_POST['edit_category'])) {
-                            $mark = $_POST['edit_mark'];
-                            $category = $_POST['edit_category'];
-                            $this->marksModel->UpdateMark($mark,$category,$mark_id);
-                        }
-                        $this->productsView->ShowLocation('admin');
-                    }else{
-                        $this->loginView->ShowLogin();
-                    }
-                }
-            }
-        ?>s->productosModel = new ProductsModel();
+            $this->productsView = new ProductsView();
+            $this->marksView = new MarksView();
+            $this->productosModel = new ProductsModel();
             $this->marksModel = new MarksModel();
+            $this->loginView = new LoginView();
+            $this->loginControl = new LoginController();
         }
-        //  MUESTRA VISTA DE LA PAGINA EDITABLE DEL ADMINISTRADOR 
-        /*function ShowLoginUsername(){
-            $marks = $this->marksModel->GetMarks();
-            $products = $this->productosModel->GetProducts();
-            $this->Productsview->ShowLoginUsername($products, $marks);
-        }*/
         //LLAMA AL HOME DE MARCAS
         function HomeMarks(){
             $marks = $this->marksModel->GetMarks();
-            $this->Marksview->ShowMarks($marks);
+            $this->marksView->ShowMarks($marks);
         }
-
         //INSERTA UNA NUEVA MARCA
         function InsertMark(){
-            $this->marksModel->InsertMark($_POST['input_mark'],$_POST['input_category']);
-            $marks = $this->marksModel->GetMarks();
-            $products = $this->productosModel->GetProducts();
-            $this->Productsview->ShowLoginUsername($products, $marks);
+            $logeado = $this->loginControl->checkLoggedIn();
+            if($logeado){
+                if (isset($_POST['input_mark']) && isset($_POST['input_category'])) {
+                    $mark = $_POST['input_mark'];
+                    $category = $_POST['input_category'];
+                    $this->marksModel->InsertMark($mark, $category);
+                }
+                $this->productsView->ShowLocation('admin');
+            }else{
+                $this->loginView->ShowLogin();
+            }
         }
         //ELIMINA UNA MARCA POR ID
         function DeleteMark($params = null){
-            $mark_id = $params[':ID'];
-            $this->marksModel->DeleteMark($mark_id);
-            $marks = $this->marksModel->GetMarks();
-            $products = $this->productosModel->GetProducts();
-            $this->Productsview->ShowLoginUsername($products, $marks);
+            $logeado = $this->loginControl->checkLoggedIn();
+            if($logeado){
+                $mark_id = $params[':ID'];
+                $this->marksModel->DeleteMark($mark_id);
+                $this->productsView->ShowLocation('admin');
+            }else{
+                $this->loginView->ShowLogin();
+            }
         }
-        //LLAMA LA VISTA PARA EDITAR UN MARCA POR ID
+        //LLAMA LA VISTA PARA EDITAR UNA MARCA POR ID
         function EditMark($params = null){
-            $mark_id = $params[':ID'];
-            $mark = $this->marksModel->GetMarkById($mark_id);
-            $this->MarksView->ShowEditMark($mark); 
+            $logeado = $this->loginControl->checkLoggedIn();
+            if($logeado){
+                $mark_id = $params[':ID'];
+                $mark = $this->marksModel->GetMarkById($mark_id);
+                $this->marksView->ShowEditMark($mark);
+            }else{
+                $this->loginView->ShowLogin();
+            }
         }
         //LLAMA A ACTUALIZAR UNA MARCA
         function UpdateMark($params = null){
-            $mark_id = $params[':ID'];
-            if (isset($_POST['edit_mark']) && isset($_POST['edit_category'])) {
-                $mark = $_POST['edit_mark'];
-                $category = $_POST['edit_category'];
-                $this->marksModel->UpdateMark($mark,$category,$mark_id);
-            }
-            $marks = $this->marksModel->GetMarks();
-            $products = $this->productosmodel->GetProducts();
-            $this->Productsview->ShowLoginUsername($products, $marks);
+            $logeado = $this->loginControl->checkLoggedIn();
+            if($logeado){
+                $mark_id = $params[':ID'];
+                if (isset($_POST['edit_mark']) && isset($_POST['edit_category'])) {
+                    $mark = $_POST['edit_mark'];
+                    $category = $_POST['edit_category'];
+                    $this->marksModel->UpdateMark($mark,$category,$mark_id);
+                }
+                $this->productsView->ShowLocation('admin');
+            }else{
+                $this->loginView->ShowLogin();
             }
         }
-?> 
+    }
+?>
--- Controller/ProductsController.php
+++ Controller/ProductsController.php
@@ -1,195 +1,106 @@
 <?php
 
-require_once "./View/ProductsView.php";
-require_once "./Model/ProductsModel.php";
-require_once "./Model/MarksModel.php";
+    require_once "./View/ProductsView.php";
+    require_once "./View/LoginView.php";
+    require_once "./Model/ProductsModel.php";
+    require_once "./Model/MarksModel.php";
+    require_once "./Controller/LoginController.php";
 
-class ProductsController{
+    class ProductsController{
 
-    private $view;
-    private $model;
-    private $marksModel;
-    
-    function __construct(){
-        $this->view = new ProductsView();
-        $this->model = new ProductsModel();
-        $this->marksModel = new MarksModel();
-    }
-      //LLAMA AL HOME
-      function Home(){
-        $marks = $this->marksModel->GetMarks();
-        $products = $this->model->GetProducts();
-        $this->view->ShowHome($products, $marks, $mark_id = null);
-    }
-    //LLAMA AL LOGIN
-    function Login(){
-        $this->view->ShowLogin();
-    }
-    //MUESTRA LA PAGINA PARA EL ADMIN
-    function LoginUsername(){
-        $marks = $this->marksModel->GetMarks();
-        $products = $this->model->GetProducts();
-        $this->view->ShowLoginUsername($products, $marks);
-    }
-    //INSERTA UN NUEVO PRODUCTO
-    function InsertProduct(){
-        $this->model->InsertProduct($_POST['input_product'],$_POST['input_price'],$_POST['input_stock'],$_POST['input_description'],$_POST['select_brand']);
-        $marks = $this->marksModel->GetMarks();
-        $products = $this->model->GetProducts();
-        $this->view->ShowLoginUsername($products, $marks);
-    }
-    //ELIMINA UN PRODUCTO POR ID
-    function DeleteProduct($params = null){
-        $product_id = $params[':ID'];
-        $this->model->DeleteProduct($product_id);
-      
-        $marks = $this->marksModel->GetMarks();
-        $products = $this->model->GetProducts();
-        $this->view->ShowLoginUsername($products, $marks);
-    }
-    //LLAMA LA VISTA PARA EDITAR UN PRODUCTO POR ID
-    function EditProduct($params = null){
-        $product_id = $params[':ID'];
-        $marks = $this->marksModel->GetMarks();
-        $product = $this->model->GetProductById($product_id);
-        $this->view->ShowEditProduct($product, $marks); 
-    }
-    //LLAMA A ACTUALIZAR UN PRODUCTO
-    function UpdateProduct($params = null){
-        $product_id = $params[':ID'];
-        if ((isset($_POST['edit_product']) && isset($_POST['edit_price'])) && (isset($_GET['edit_stock']) && isset($_GET['edit_description'])) && isset($_GET['select_brand'])) {
-            $product = $_POST['edit_product'];
-            $price = $_POST['edit_price'];
-            $stock = $_POST['edit_stock'];
-            $description = $_POST['edit_description'];
-            $brand = $POST['select_brand'];
-            $this->model->UpdateProduct($product,$price,$stock,$description,$brand,$product_id);
+        private $view;
+        private $model;
+        private $marksModel;
+        private $loginView;
+        private $loginControl;
+
+        function __construct(){
+            $this->view = new ProductsView();
+            $this->model = new ProductsModel();
+            $this->marksModel = new MarksModel();
+            $this->loginView = new LoginView();
+            $this->loginControl = new LoginController();
         }
-        $marks = $this->marksModel->GetMarks();
-        $products = $this->model->GetProducts();
-        $this->view->ShowLoginUsername($products, $marks);
-            
-        } 
-
-    //LLAMA AL FILTRO DE LOS PRODUCTOS POR MARCA
-    function FilterProductsByMark(){
-        if (isset($_POST['select_brand'])) {
-            $mark_id = $_POST['select_brand'];
-            $products = $<?php
-
-            require_once "./View/ProductsView.php";
-            require_once "./View/LoginView.php";
-            require_once "./Model/ProductsModel.php";
-            require_once "./Model/MarksModel.php";
-            require_once "./Controller/LoginController.php";
-        
-            class ProductsController{
-        
-                private $view;
-                private $model;
-                private $marksModel;
-                private $loginView;
-                private $loginControl;
-        
-                function __construct(){
-                    $this->view = new ProductsView();
-                    $this->model = new ProductsModel();
-                    $this->marksModel = new MarksModel();
-                    $this->loginView = new LoginView();
-                    $this->loginControl = new LoginController();
-                }
-                //LLAMA AL HOME
-                function Home(){
-                    $marks = $this->marksModel->GetMarks();
-                    $products = $this->model->GetProducts();
-                    $this->view->ShowHome($products, $marks);
-                }
-                //INSERTA UN NUEVO PRODUCTO
-                function InsertProduct(){
-                    $logeado = $this->loginControl->checkLoggedIn();
-                    if($logeado){
-                        if ((isset($_POST['input_product']) && isset($_POST['input_price'])) && (isset($_POST['input_stock']) && isset($_POST['input_description'])) && isset($_POST['select_brand'])) {
-                            $product = $_POST['input_product'];
-                            $price = $_POST['input_price'];
-                            $stock = $_POST['input_stock'];
-                            $description = $_POST['input_description'];
-                            $brand =  $_POST['select_brand'];
-                        $this->model->InsertProduct($product,$price,$stock,$description,$brand);
-                        }
-                        $this->view->ShowLocation('admin');
-                    }else{
-                        $this->loginView->Login();
-                    }
-                }
-                //ELIMINA UN PRODUCTO POR ID
-                function DeleteProduct($params = null){
-                    $logeado = $this->loginControl->checkLoggedIn();
-                    if($logeado){
-                        $product_id = $params[':ID'];
-                        $this->model->DeleteProduct($product_id);
-                        $this->view->ShowLocation('admin');
-                    }else{
-                        $this->loginView->ShowLogin();
-                    }
-                }
-                //LLAMA LA VISTA PARA EDITAR UN PRODUCTO POR ID
-                function EditProduct($params = null){
-                    $logeado = $this->loginControl->checkLoggedIn();
-                    if($logeado){
-                        $product_id = $params[':ID'];
-                        $marks = $this->marksModel->GetMarks();
-                        $product = $this->model->GetProductById($product_id);
-                        $this->view->ShowEditProduct($product, $marks); 
-                    }else{
-                        $this->loginView->ShowLogin();
-                    }
-                }
-                //LLAMA A ACTUALIZAR UN PRODUCTO
-                function UpdateProduct($params = null){
-                    $logeado = $this->loginControl->checkLoggedIn();
-                    if($logeado){
-                        $product_id = $params[':ID'];
-                        if ((isset($_POST['edit_product']) && isset($_POST['edit_price'])) && (isset($_POST['edit_stock']) && isset($_POST['edit_description'])) && isset($_POST['select_brand'])) {
-                            $product = $_POST['edit_product'];
-                            $price = $_POST['edit_price'];
-                            $stock = $_POST['edit_stock'];
-                            $description = $_POST['edit_description'];
-                            $brand = $_POST['select_brand'];
-                            $this->model->UpdateProduct($product,$price,$stock,$description,$brand,$product_id);
-                        }
-                        $this->view->ShowLocation('admin');
-                    }else{
-                        $this->loginView->ShowLogin();
-                    }
-                }
-                //LLAMA AL FILTRO DE LOS PRODUCTOS POR MARCA
-                function FilterProductsByMark(){
-                    if (isset($_POST['select_brand'])) {
-                        $mark_id = $_POST['select_brand'];
-                        $products = $this->model->GetProductsByMark($mark_id);
-                        $marks = $this->marksModel->GetMarks();
-                        $this->view->ShowHome($products, $marks);
-                    }
-                }
-                //LLAMA A LA VISTA EN DETALLE DE UN PRODUCTO
-                function ItemDetail($params = null){
-                    $product_id = $params[':ID'];
-                    $product = $this->model->GetProductById($product_id);
-                    $mark_id = $product->id_marca;
-                    $mark = $this->marksModel->GetMarkById($mark_id);
-                    $this->view->ShowItemDetail($product, $mark); 
-                }
-            }
-        ?>this->model->GetProductsByMark($mark_id);
+        //LLAMA AL HOME
+        function Home(){
             $marks = $this->marksModel->GetMarks();
-            $this->view->ShowHome($products, $marks, $mark_id);
+            $products = $this->model->GetProducts();
+            $this->view->ShowHome($products, $marks);
         }
-   }  //LLAMA A LA VISTA EN DETALLE DE UN PRODUCTO
-   function ItemDetail($params = null){
-       $product_id = $params[':ID'];
-       $product = $this->model->GetProductById($product_id);
-       $mark_id = $product->id_marca;
-       $mark = $this->marksModel->GetMarkById($mark_id);
-       $this->view->ShowItemDetail($product, $mark); 
-   }
-}
+        //INSERTA UN NUEVO PRODUCTO
+        function InsertProduct(){
+            $logeado = $this->loginControl->checkLoggedIn();
+            if($logeado){
+                if ((isset($_POST['input_product']) && isset($_POST['input_price'])) && (isset($_POST['input_stock']) && isset($_POST['input_description'])) && isset($_POST['select_brand'])) {
+                    $product = $_POST['input_product'];
+                    $price = $_POST['input_price'];
+                    $stock = $_POST['input_stock'];
+                    $description = $_POST['input_description'];
+                    $brand =  $_POST['select_brand'];
+                $this->model->InsertProduct($product,$price,$stock,$description,$brand);
+                }
+                $this->view->ShowLocation('admin');
+            }else{
+                $this->loginView->Login();
+            }
+        }
+        //ELIMINA UN PRODUCTO POR ID
+        function DeleteProduct($params = null){
+            $logeado = $this->loginControl->checkLoggedIn();
+            if($logeado){
+                $product_id = $params[':ID'];
+                $this->model->DeleteProduct($product_id);
+                $this->view->ShowLocation('admin');
+            }else{
+                $this->loginView->ShowLogin();
+            }
+        }
+        //LLAMA LA VISTA PARA EDITAR UN PRODUCTO POR ID
+        function EditProduct($params = null){
+            $logeado = $this->loginControl->checkLoggedIn();
+            if($logeado){
+                $product_id = $params[':ID'];
+                $marks = $this->marksModel->GetMarks();
+                $product = $this->model->GetProductById($product_id);
+                $this->view->ShowEditProduct($product, $marks); 
+            }else{
+                $this->loginView->ShowLogin();
+            }
+        }
+        //LLAMA A ACTUALIZAR UN PRODUCTO
+        function UpdateProduct($params = null){
+            $logeado = $this->loginControl->checkLoggedIn();
+            if($logeado){
+                $product_id = $params[':ID'];
+                if ((isset($_POST['edit_product']) && isset($_POST['edit_price'])) && (isset($_POST['edit_stock']) && isset($_POST['edit_description'])) && isset($_POST['select_brand'])) {
+                    $product = $_POST['edit_product'];
+                    $price = $_POST['edit_price'];
+                    $stock = $_POST['edit_stock'];
+                    $description = $_POST['edit_description'];
+                    $brand = $_POST['select_brand'];
+                    $this->model->UpdateProduct($product,$price,$stock,$description,$brand,$product_id);
+                }
+                $this->view->ShowLocation('admin');
+            }else{
+                $this->loginView->ShowLogin();
+            }
+        }
+        //LLAMA AL FILTRO DE LOS PRODUCTOS POR MARCA
+        function FilterProductsByMark(){
+            if (isset($_POST['select_brand'])) {
+                $mark_id = $_POST['select_brand'];
+                $products = $this->model->GetProductsByMark($mark_id);
+                $marks = $this->marksModel->GetMarks();
+                $this->view->ShowHome($products, $marks);
+            }
+        }
+        //LLAMA A LA VISTA EN DETALLE DE UN PRODUCTO
+        function ItemDetail($params = null){
+            $product_id = $params[':ID'];
+            $product = $this->model->GetProductById($product_id);
+            $mark_id = $product->id_marca;
+            $mark = $this->marksModel->GetMarkById($mark_id);
+            $this->view->ShowItemDetail($product, $mark); 
+        }
+    }
+?>
