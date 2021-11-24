4e7c0793de2b039242f96103180198c8f91fc6df
constanza30101 <comartinez@alumnos.exa.unicen.edu.ar>
2021-11-24 10:49:33 -0300

    bd actualizada

--- carrito_de_compras.sql
+++ carrito_de_compras.sql
@@ -3,18 +3,20 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-11-2020 a las 17:08:18
--- Tiempo de generación: 04-11-2020 a las 19:22:50
+-- Tiempo de generación: 24-11-2021 a las 14:48:58
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Base de datos: `carrito_de_compras`
 --
@@ -26,74 +28,124 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `comentario` (
-  `id_comentario` int(50) NOT NULL,
+  `id_comentario` int(11) NOT NULL,
   `comentario` varchar(200) NOT NULL,
-  `id_usuario` int(50) NOT NULL,
+  `valoracion` int(1) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `comentario`
+--
+
+INSERT INTO `comentario` (`id_comentario`, `comentario`, `valoracion`, `id_usuario`, `id_producto`) VALUES
+(4, 'bien', 3, 4, 1),
+(5, '3545', 1, 4, 1),
+(6, '44', 5, 4, 1),
+(7, '0000', 5, 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagen`
+--
+
+CREATE TABLE `imagen` (
+  `id` int(50) NOT NULL,
+  `imagen` varchar(200) NOT NULL,
   `id_producto` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `imagen`
+--
+
+INSERT INTO `imagen` (`id`, `imagen`, `id_producto`) VALUES
+(1, 'img/collar.jpg', 1),
+(2, 'img/collar2.jpg', 1),
+(3, 'img/collar3.jpg', 1),
+(4, 'img/aros2.jpg', 2),
+(5, 'img/aros3.jpg', 2),
+(6, 'img/colita.jpg', 2),
+(7, 'img/aros2.jpg', 11);
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `marca`
 --
+
 CREATE TABLE `marca` (
   `id_marca` int(11) NOT NULL,
   `marca` varchar(100) NOT NULL,
   `categoria` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Volcado de datos para la tabla `marca`
 --
+
 INSERT INTO `marca` (`id_marca`, `marca`, `categoria`) VALUES
 (1, 'donadonna', 'Bijouterie Online'),
 (2, 'Lunera acero', 'bijouterie mayorista'),
 (3, 'gotergood', 'pulseras y brazaletes para hombres'),
-(4, 'Rapsodia', 'variedad de accesorios'),
-(5, 'Tiffany & Co.', 'agregada desde la pagina');
+(4, 'Rapsodia', 'variedad de accesorios');
+
 -- --------------------------------------------------------
+
 --
 -- Estructura de tabla para la tabla `producto`
 --
+
 CREATE TABLE `producto` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `precio` int(11) NOT NULL,
   `stock` int(11) NOT NULL,
   `descripcion` varchar(200) NOT NULL,
-  `imagen` varchar(50) NOT NULL,
   `id_marca` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Volcado de datos para la tabla `producto`
 --
-INSERT INTO `producto` (`id`, `nombre`, `precio`, `stock`, `descripcion`, `imagen`, `id_marca`) VALUES
-(1, 'pulsera', 205, 23, 'pulsera elegante', 'img/pulsera1.jpg', 4),
-(2, 'aros', 100, 20, 'aros perfecto para fiesta de noche', 'img/aros2.jpg', 2),
-(3, 'pulsera de cuentas', 200, 15, 'colores bien brillantes', 'img/pulsera4.jpg', 5),
-(4, 'colita fringes', 100, 8, 'ajuste delicado', '', 4),
-(5, 'aros square', 300, 5, 'ideal para ir a una quinta', '', 1),
-(6, 'mix choker+colgante', 300, 3, 'oferta imperdible', '', 2),
-(7, 'collar bull', 300, 5, 'genial para todos los dias', 'img/pulsera7.jpg', 1),
-(8, 'Collar zafiro', 240, 23, 'el colla de la marca', '', 4),
-(9, 'aros hanna', 12122, 12, 'agregado desde la pagina', '', 5);
+
+INSERT INTO `producto` (`id`, `nombre`, `precio`, `stock`, `descripcion`, `id_marca`) VALUES
+(1, 'pulsera', 205, 23, 'pulsera elegante', 4),
+(2, 'colita fringes', 100, 8, 'ajuste delicado', 4),
+(3, 'aros square', 300, 11, 'ideal para ir a una quinta', 1),
+(4, 'collar bull', 300, 5, 'genial para todos los dias', 1),
+(5, 'Collar Square', 350, 16, 'Ideal para fiesta de noche', 2),
+(6, 'Aros Hanna', 500, 5, 'Aros abridores de oro', 3),
+(7, 'Pulsera T&M', 450, 18, 'Ideal para una entrevista', 3),
+(8, 'Anillos', 150, 14, 'Anillos de pareja', 1),
+(9, 'Tobillera', 420, 22, 'Ideal para la playa', 2),
+(10, 'Collar Zafiro', 200, 21, 'Elegante', 2),
+(11, 'Collar Zafiro', 250, 11, 'Fino', 1);
+
 -- --------------------------------------------------------
+
 --
 -- Estructura de tabla para la tabla `usuario`
 --
+
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(250) NOT NULL,
   `admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Volcado de datos para la tabla `usuario`
 --
+
 INSERT INTO `usuario` (`id`, `email`, `password`, `admin`) VALUES
-(1, 'magamedico@gmail.com', '$2y$10$zJCL282zDJdwSfggMMHSNuikf0hxDZiwz9KckRW/G8D/ACxo8buq2', 1),
-(2, 'usuario@publico', '$2y$10$zJCL282zDJdwSfggMMHSNuikf0hxDZiwz9KckRW/G8D/ACxo8buq2', 0),
-(3, 'nuevo@publico', '$2y$10$2zE98Ld1qoyvldSyucCKYOda2CcVVvUAYUIBJyeyMZlzg8f.haE8i', 0);
+(2, 'usuario@hotmail.com', '$2y$10$tAs6KneNjG6.v4Pz1O/93eiPxZZhWeszzHM5zBPz13ZHpu1R9RnGu', 0),
+(3, 'prueba@hotmail.com', '$2y$10$BSX0ctLE8ubJKWfW8HDCCORbEkTChHeH2VZ8akqQ4F7AWCK4N40lK', 0),
+(4, 'cotymar@gmail.com', '$2y$10$RyPs73kbgOH0VUwjMSEeQOJlyt53RNyPdYhqB0eGtHedAah0lTNN.', 1);
+
 --
 -- Índices para tablas volcadas
 --
@@ -107,21 +159,33 @@ ALTER TABLE `comentario`
   ADD KEY `id_producto` (`id_producto`);
 
 --
+-- Indices de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
 -- Indices de la tabla `marca`
 --
 ALTER TABLE `marca`
   ADD PRIMARY KEY (`id_marca`);
+
 --
 -- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_marca` (`id_marca`);
+  ADD KEY `id_marca` (`id_marca`),
+  ADD KEY `id` (`id`),
+  ADD KEY `id_2` (`id`);
+
 --
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
+
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
@@ -130,23 +194,32 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `comentario`
 --
 ALTER TABLE `comentario`
-  MODIFY `id_comentario` int(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `marca`
 --
 ALTER TABLE `marca`
-  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- Restricciones para tablas volcadas
 --
@@ -155,8 +228,14 @@ ALTER TABLE `usuario`
 -- Filtros para la tabla `comentario`
 --
 ALTER TABLE `comentario`
-  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD CONSTRAINT `imagen_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `producto`
@@ -164,6 +243,7 @@ ALTER TABLE `comentario`
 ALTER TABLE `producto`
   ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_marca`) REFERENCES `marca` (`id_marca`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
